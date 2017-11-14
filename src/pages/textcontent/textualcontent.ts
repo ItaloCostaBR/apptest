b7315ae86108661782cf96ab5823006c6e423555
Italo Costa <italo.costa@csktech.com.br>
2017-11-14 16:43:31 -0300

    alter

---
+++ src/pages/textcontent/textualcontent.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'textualcontent-page',
+  templateUrl: 'textualcontent.html'
+})
+export class TextualContent {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
